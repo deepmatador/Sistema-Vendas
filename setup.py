8ac5bf9ed3a0783833901a4d6568b731fb38a9a9
Mateus Haccourt Woikolesko <142453310+deepmatador@users.noreply.github.com>
2025-02-19 18:49:04 -0300

    V.1

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
 from functions.Login import logar
 from view.uses.FRMlogin import Ui_login
 
